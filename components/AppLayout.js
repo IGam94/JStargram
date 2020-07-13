758b6bff12384c24cf3729f03b518968492f3bfc
LEE <kosnsn@gmail.com>
2020-07-13 13:45:31 +0900

    test

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -28,7 +28,7 @@ const AppLayout = ({children}) => {
             </Menu>
             <Row>
                 <Col xs={24} md={6}>
-                    {isLoggedIn ? <UserProfile /> : <LoginForm />}
+                    {isLoggedIn ? <UserProfile /> : <LoginForm setIsLoggedIn={setIsLoggedIn}/>}
                     </Col>
                 <Col xs={24} md={12}>
                     {children}
